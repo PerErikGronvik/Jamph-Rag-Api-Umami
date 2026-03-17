d06e93c401199531ae7805b6597d873399dd3de7
Aryokh04 <143172110+Aryokh04@users.noreply.github.com>
2026-03-17 12:59:51 +0100

    Revert "test"
    This reverts commit da314781ef3357e6ea69c310e7e103cf34c5cac0.

--- src/test/kotlin/no/jamph/llmValidation/EstimateCostSqlTest.kt
+++ src/test/kotlin/no/jamph/llmValidation/EstimateCostSqlTest.kt
@@ -8,6 +8,6 @@ class EstimateCostSqlTest {
     
     @Test
     fun `invalid SQL returns 0`() {
-        assertEquals(0.0, estimateCostInMB("NOT VALID SQL!!!"))
+        assertEquals(, actual)
     }
 }
