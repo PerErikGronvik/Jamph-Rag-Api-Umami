42eac5f7c7cae25ba1144a6414f7aa95142df3c5
Aryokh04 <143172110+Aryokh04@users.noreply.github.com>
2026-03-17 12:38:52 +0100

    start implementing tests

--- src/test/kotlin/no/jamph/llmValidation/EstimateCostSqlTest.kt
+++ src/test/kotlin/no/jamph/llmValidation/EstimateCostSqlTest.kt
@@ -1,1 +1,13 @@
 package no.jamph.llmValidation
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class EstimateCostSqlTest {
+    
+    @Test
+    fun `invalid SQL returns 0`() {
+        assertEquals(, actual)
+    }
+}
